13edd0cdd8cc6f11c181207477c85c7c418725c0
D0lan <aryan_mj7@yahoo.com>
2019-11-02 17:44:34 -0400

    ethereum

---
+++ spring/ethereum/contract.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.5.11;
+
+contract BCMess{
+    event sendMessageEvent(address indexed from, address indexed to,bytes message, bytes32 encryption);
+    event profileUpdateEvent(address indexed from, bytes32 name)
+
+    struct User {
+        uint messageStartBlock;
+        bool isUser;
+        int trustLevel;
+        bytes32 publicKey;
+        bytes32 name;
+    }
+
+    mapping (address=> User) public users;
+
+    function sendMessage(address to, bytes memory message, bytes32 encryption) public onlyMember {
+        emit sendMessageEvent(msg.sender, to, message, encryption);
+    }
+
+    function join(bytes32 publicKeyLeft, bytes32 publicKeyRight) public onlyMember {
+        require(members[msg.sender].isMember==false);
+        User memory newUser= User(publicKeyLeft, publicKeyRight, "", "", 0, true);
+        users[msg.sender]=newUser;
+    }
+
+    function updateProfile(bytes32 name, bytes32 avatarUrl) public onlyMember {
+        users[msg.sender].name = name;
+        emit profileUpdateEvent(msg.sender, name, avatarUrl);
+    }
+
+    modifier onlyMember(){
+        require(users[msg.sender].isUser==true);
+        _;
+    }
+}
